109c4dd1e1ad0cd42d0ac846a1b40aceceb20e3a
doter <doter@192.168.12.22>
2019-03-18 19:59:42 +0100

    bootstrap y map afegit

--- public_html/JSON_API.js
+++ public_html/JSON_API.js
@@ -7,6 +7,8 @@
 //Variables globals
 var parat = false;
 var frequenciaActualitzacio = 3;
+var contingut = document.getElementById("contingut");
+var mapa = document.getElementById("mapid");
 
 //document
 var tempsLabel = document.getElementById("segons");
@@ -83,8 +85,30 @@ function actualitzaDadesPantalla(data)
         linea = linea + "<br>Estació tipus:" + tipo + " hi ha <strong>" + arrayTipus[tipo] + "</strong> estacions amb <strong>" + arrayBicisPerTipus[tipo] + "</strong> bicis disponibles i <strong>" + arraySlots[tipo] + "</strong> slots lliures.";
     }
     //afegueixo contingut dins html
-    $("#contingut").append(linea + "</p>");
+    contingut.innerHTML=(linea + "</p>");
 }
 
 
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+//var circle = L.circle([51.508, -0.11], {
+//    color: 'red',
+//    fillColor: '#f03',
+//    fillOpacity: 0.5,
+//    radius: 500
+//}).addTo(mymap);
+//
+//var polygon = L.polygon([
+//    [51.509, -0.08],
+//    [51.503, -0.06],
+//    [51.51, -0.047]
+//]).addTo(mymap);
